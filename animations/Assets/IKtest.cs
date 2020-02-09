104f97b971736266093f9c834aa40e4516f22235
Skoukalir <dom.co@hotmail.com>
2020-02-09 18:11:24 +1100

    add animations project

---
+++ animations/Assets/IKtest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKtest : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject target;
+    void OnAnimatorIK()
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1f);
+        anim.SetIKPosition(AvatarIKGoal.RightHand, target.transform.position);
+        anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1f);
+        anim.SetIKRotation(AvatarIKGoal.RightHand, target.transform.rotation);
+    }
+}
